f4f84b57fc3ecd35461e047ea439d0edb0d51a67
muerbingsha <511071007@qq.com>
2018-08-20 14:04:51 +0800

    Create Point.h

---
+++ Point.h
@@ -0,0 +1,21 @@
+//
+//  Point.h
+//  TriMatch
+//
+//  Created by sj on 2018/7/7.
+//  Copyright © 2018 sj. All rights reserved.
+//
+
+#ifndef Point_h
+#define Point_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <OpenGLES/ES3/gl.h>
+
+@interface RoundPointView : UIView
+
+@end
+
+
+#endif /* Point_h */
